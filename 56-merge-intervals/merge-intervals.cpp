d66beb5576413242c5770cbdaa0ce08a87c682ad
Anmol Sharma <148962440+anmolsharma170@users.noreply.github.com>
2026-08-12 21:46:11 +0530

    Time: 10 ms (13.48%) | Memory: 24.8 MB (10.67%) - LeetSync

---
+++ 56-merge-intervals/merge-intervals.cpp
@@ -0,0 +1,24 @@
+    bool cmp(vector<int> &a,vector<int> &b){
+        return a[1]<b[1];
+    }
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        sort(intervals.begin(),intervals.end(),cmp);
+        vector<vector<int>> result;
+        int n = intervals.size();
+        result.push_back(intervals[n-1]);
+        for(int i=n-2;i>=0;i--){
+            auto curr = intervals[i];
+            if(curr[1]>=result[result.size()-1][0]){
+                result[result.size()-1][0]=min(curr[0],result[result.size()-1][0]);
+                result[result.size()-1][1]=max(curr[1],result[result.size()-1][1]);
+            }
+            else{
+                result.push_back(intervals[i]);
+            }
+        }
+        reverse(result.begin(),result.end());
+        return result;
+    }
+};

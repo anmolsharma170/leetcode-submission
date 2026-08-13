5bc900a5b936cf3c69d1c510342e685424fa2961
Anmol Sharma <148962440+anmolsharma170@users.noreply.github.com>
2026-08-13 10:33:48 +0530

    Time: 9 ms (16.08%) | Memory: 24.9 MB (6.81%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -1,24 +1,21 @@
-    bool cmp(vector<int> &a,vector<int> &b){
-        return a[1]<b[1];
-    }
+bool cmp(vector<int> &a,vector<int> &b){
+    return a[0]<b[0];
+}
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        sort(intervals.begin(),intervals.end(),cmp);
         vector<vector<int>> result;
+        sort(intervals.begin(),intervals.end(),cmp);
         int n = intervals.size();
-        result.push_back(intervals[n-1]);
-        for(int i=n-2;i>=0;i--){
-            auto curr = intervals[i];
-            if(curr[1]>=result[result.size()-1][0]){
-                result[result.size()-1][0]=min(curr[0],result[result.size()-1][0]);
-                result[result.size()-1][1]=max(curr[1],result[result.size()-1][1]);
+        result.push_back(intervals[0]);
+        for(int i=1;i<n;i++){
+            vector<int> curr = intervals[i];
+            if(curr[0]<=result[result.size()-1][1]){
+                result[result.size()-1][0]=min(result[result.size()-1][0],curr[0]); 
+                result[result.size()-1][1]=max(result[result.size()-1][1],curr[1]); 
             }
-            else{
-                result.push_back(intervals[i]);
-            }
+            else result.push_back(intervals[i]);
         }
-        reverse(result.begin(),result.end());
         return result;
     }
 };

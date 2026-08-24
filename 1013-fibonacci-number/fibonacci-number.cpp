1ba28518b5d87f1d449d89d2ea2874ba7a67b5fa
Anmol Sharma <148962440+anmolsharma170@users.noreply.github.com>
2026-08-24 18:28:53 +0530

    Time: 3 ms (64.76%) | Memory: 8.3 MB (11.46%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int fibo(vector<int> &dp,int n){
+    int fibo(vector<int> &dp,int n){   // dp+memoization
         if(n<=1) return n;
         if(dp[n]!=-1) return dp[n];
         dp[n]=fibo(dp,n-1)+fibo(dp,n-2);

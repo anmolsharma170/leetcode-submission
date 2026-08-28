6245d150f502ee9cdfe03deeadb48bb9180ed3ca
Anmol Sharma <148962440+anmolsharma170@users.noreply.github.com>
2026-08-28 10:48:29 +0530

    Time: 0 ms (100.00%) | Memory: 13.5 MB (9.01%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -10,18 +10,11 @@ public:
                 }
                 else if(j==i) dp[i][j]= dp[i-1][j-1]+triangle[i][j];
                 else{
-                    // dp[i][j]=min(dp[i-1][j-1]+triangle[i][j],dp[i-1][j-1]+triangle[i][j+1]);
                     dp[i][j] = min(dp[i-1][j-1], dp[i-1][j]) + triangle[i][j];
                 }
             }
         }
         int n=dp.size();
-        for(int i = 0;i<dp.size();i++){
-            for(int j = 0;j<dp[i].size();j++){
-                cout<<dp[i][j]<<" ";
-            }
-            cout<<endl;
-        }
         int minele = INT_MAX;
         for(int j = 0;j<dp[n-1].size();j++){
             minele = min(dp[n-1][j],minele);

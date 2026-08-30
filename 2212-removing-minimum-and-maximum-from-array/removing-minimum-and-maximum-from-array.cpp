13b172c9b945982c89fb635b2138f280c1d303aa
Anmol Sharma <148962440+anmolsharma170@users.noreply.github.com>
2026-08-30 17:18:06 +0530

    Time: 0 ms (100.00%) | Memory: 92.2 MB (22.14%) - LeetSync

--- 2212-removing-minimum-and-maximum-from-array/removing-minimum-and-maximum-from-array.cpp
+++ 2212-removing-minimum-and-maximum-from-array/removing-minimum-and-maximum-from-array.cpp
@@ -3,8 +3,6 @@ public:
     int minimumDeletions(vector<int>& nums) {
         cout<<nums.size()<<endl;
         if(nums.size()==1 || nums.size()==2) return nums.size();
-        int count1 = 0;
-        int count2=0;
         int minele=INT_MAX,maxele =INT_MIN,minidx=-1,maxidx=-1;
         for(int i = 0;i<nums.size();i++){
             if(nums[i]<minele){
@@ -16,7 +14,6 @@ public:
                 maxidx = i;
             }
         }
-        cout<<minidx<<"  "<<maxidx<<endl;
         int onlyfront = 0;
         if(minidx>maxidx){
             onlyfront = minidx+1;
@@ -28,25 +25,19 @@ public:
         }
         else onlyback= nums.size()-maxidx;
         if(minidx<nums.size()/2 && maxidx<nums.size()/2){
-            cout<<"first"<<endl;
             if(minidx>maxidx) return minidx+1;
             else return maxidx+1;
         }
         if(minidx>nums.size()/2){
-            cout<<"second"<<endl;
             minidx = nums.size()-minidx;
         }
         else{
-            cout<<"min idx is "<<minidx; 
-            cout<<"\nthird"<<endl;
             minidx = minidx+1;
         } 
         if(maxidx>nums.size()/2){
             maxidx = nums.size()-maxidx;
         }
         else maxidx+=1;
-        cout<< maxidx <<"->"<< maxele <<" "<<minidx<<"->"<<minele;
-        
         return min({minidx+maxidx,onlyfront,onlyback}) ;
     }
 };

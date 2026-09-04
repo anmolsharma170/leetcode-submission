b184ed4adba7224353264a1fd3ba692b5c8c5d80
Anmol Sharma <148962440+anmolsharma170@users.noreply.github.com>
2026-09-04 16:59:45 +0530

    Time: 0 ms (100.00%) | Memory: 15.4 MB (5.28%) - LeetSync

--- 1646-kth-missing-positive-number/kth-missing-positive-number.cpp
+++ 1646-kth-missing-positive-number/kth-missing-positive-number.cpp
@@ -8,13 +8,9 @@ public:
         for(auto ele: arr) st.insert(ele);
         for(int i = 1;i<=end;i++){
             if(st.find(i)==st.end()){
-                cout<<"inside1 "<<i<<endl;
                 st.insert(i);
                 count++;
-                cout<<"Count increased"<<count<<endl;
-            } 
-            else{
-                cout<<"inside2 "<<i<<endl;
+                
             } 
             if(count==k) return i;
         }

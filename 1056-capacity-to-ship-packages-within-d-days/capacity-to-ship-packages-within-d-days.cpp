43bcacb1c58ac772d0f733de00064d438570d052
Anmol Sharma <148962440+anmolsharma170@users.noreply.github.com>
2026-09-11 16:22:47 +0530

    Time: 12 ms (50.22%) | Memory: 35.2 MB (19.88%) - LeetSync

--- 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
-    bool canship(vector<int> &weights,int days,int capacity){
-        int day=1,curr=0;
-        for(int w:weights){
-            if(curr+w>capacity){
+    bool canShip(vector<int>& weights, int days,int capacity){
+        int day = 1,curr = 0;
+        for(int w: weights){
+            if(w+curr>capacity){
                 day++;
                 curr=w;
             }
@@ -11,24 +11,24 @@ public:
                 curr+=w;
             }
         }
-        if(day<=days) return true;
-        return false;
+        if(day>days) return false;
+        return true;
     }
     int shipWithinDays(vector<int>& weights, int days) {
-        int lo=INT_MIN,hi=0;
-        for(int i=0;i<weights.size();i++){
-            lo=max(lo,weights[i]);
-            hi+=weights[i];
+        int lo = INT_MIN, high = 0;
+        for(int i = 0;i<weights.size();i++){
+            lo = max(lo,weights[i]);
+            high+=weights[i];
         }
-            int ans = -1;
-        while(lo<=hi){
-            int mid = lo+(hi-lo)/2;
-            if(canship(weights,days,mid)){
+        int ans = -1;
+        while(lo<=high){
+            int mid = lo+(high-lo)/2;
+            if(canShip(weights,days,mid)){
                 ans = mid;
-                hi=mid-1;
+                high = mid-1;
             }
             else{
-                lo=mid+1;
+                lo = mid+1;
             }
         }
         return ans;

ddf01bd9ca630154fabfaf1e9de0c8980491760c
Anmol Sharma <148962440+anmolsharma170@users.noreply.github.com>
2026-09-17 20:18:55 +0530

    Time: 3 ms (29.72%) | Memory: 18.6 MB (14.48%) - LeetSync

--- 1104-coloring-a-border/coloring-a-border.cpp
+++ 1104-coloring-a-border/coloring-a-border.cpp
@@ -1,34 +1,31 @@
 class Solution {
 public:
-    vector<pair<int,int>> internal;
-    void dfs(vector<vector<int>>& grid,int row,int col,int color,int initialColor){
-        int n = grid.size();
-        int m = grid[0].size();
-        if(row<0 || col<0 || row>=n || col>=m || grid[row][col]!=initialColor) return;
-        grid[row][col]=-color;
-        dfs(grid,row+1,col,color,initialColor);
-        dfs(grid,row-1,col,color,initialColor);
-        dfs(grid,row,col+1,color,initialColor);
-        dfs(grid,row,col-1,color,initialColor);
-        if(!(row==0 || col==0 || row==n-1 || col==m-1 || grid[row][col-1]!= -color || grid[row][col+1]!= -color ||grid[row-1][col]!= -color ||grid[row+1][col]!= -color)){
-            internal.push_back({row,col});
+    vector<vector<int>> internals;
+    void dfs(vector<vector<int>>& grid, int row, int col, int newcolor,int oldcolor){
+        if(row<0 || col<0 || row>=grid.size() || col>=grid[0].size()) return;
+        if(grid[row][col]!=oldcolor) return;
+        grid[row][col]= -newcolor;
+        dfs(grid,row+1,col,newcolor,oldcolor);
+        dfs(grid,row-1,col,newcolor,oldcolor);
+        dfs(grid,row,col+1,newcolor,oldcolor);
+        dfs(grid,row,col-1,newcolor,oldcolor);
+        if(!(row==0 || col==0 || row==grid.size()-1 || col==grid[0].size()-1 || grid[row-1][col]!= -newcolor || grid[row+1][col]!= -newcolor || grid[row][col-1]!= -newcolor || grid[row][col+1]!= -newcolor)){
+            internals.push_back({row,col});
         }
     }
     vector<vector<int>> colorBorder(vector<vector<int>>& grid, int row, int col, int color) {
-        int initialColor = grid[row][col];
-        if(initialColor==color) return grid;
-        dfs(grid,row,col,color,initialColor);
-        for(auto p:internal){
-            int f = p.first;
-            int s = p.second;
-            cout<<f<<" "<<s<<endl;
-            grid[f][s]=initialColor;
+        int originalColor = grid[row][col];
+        if(originalColor==color) return grid;
+        dfs(grid,row,col,color,originalColor);
+        for(auto p:internals){
+            int row = p[0];
+            int col = p[1];
+            cout<<row<<" "<<col<<endl;
+            grid[row][col]=originalColor;
         }
         for(int i = 0;i<grid.size();i++){
-            for(int j=0;j<grid[0].size();j++){
-                if(grid[i][j]<0){
-                    grid[i][j]*=-1;
-                }
+            for(int j = 0;j<grid[0].size();j++){
+                if(grid[i][j]<0) grid[i][j]*=-1;
             }
         }
         return grid;

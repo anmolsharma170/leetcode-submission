bc06fa843af3fa616825641ff6b76331f8c18fb0
Anmol Sharma <148962440+anmolsharma170@users.noreply.github.com>
2026-09-18 16:45:53 +0530

    Time: 3 ms (29.66%) | Memory: 18.4 MB (21.67%) - LeetSync

--- 1104-coloring-a-border/coloring-a-border.cpp
+++ 1104-coloring-a-border/coloring-a-border.cpp
@@ -1,27 +1,26 @@
 class Solution {
 public:
     vector<vector<int>> internals;
-    void dfs(vector<vector<int>>& grid, int row, int col, int newcolor,int oldcolor){
+    void dfs(vector<vector<int>> &grid,int row,int col,int prevcolor,int newcolor){
         if(row<0 || col<0 || row>=grid.size() || col>=grid[0].size()) return;
-        if(grid[row][col]!=oldcolor) return;
+        if(grid[row][col]!=prevcolor) return;
         grid[row][col]= -newcolor;
-        dfs(grid,row+1,col,newcolor,oldcolor);
-        dfs(grid,row-1,col,newcolor,oldcolor);
-        dfs(grid,row,col+1,newcolor,oldcolor);
-        dfs(grid,row,col-1,newcolor,oldcolor);
-        if(!(row==0 || col==0 || row==grid.size()-1 || col==grid[0].size()-1 || grid[row-1][col]!= -newcolor || grid[row+1][col]!= -newcolor || grid[row][col-1]!= -newcolor || grid[row][col+1]!= -newcolor)){
+        dfs(grid,row+1,col,prevcolor,newcolor);
+        dfs(grid,row-1,col,prevcolor,newcolor);
+        dfs(grid,row,col+1,prevcolor,newcolor);
+        dfs(grid,row,col-1,prevcolor,newcolor);
+        if(!(row==0 || col==0 || row==grid.size()-1 || col==grid[0].size()-1 || grid[row-1][col]!= -newcolor || grid[row+1][col]!= -newcolor|| grid[row][col-1]!= -newcolor|| grid[row][col+1]!= -newcolor)){
             internals.push_back({row,col});
         }
     }
     vector<vector<int>> colorBorder(vector<vector<int>>& grid, int row, int col, int color) {
-        int originalColor = grid[row][col];
-        if(originalColor==color) return grid;
-        dfs(grid,row,col,color,originalColor);
+        int prevcolor = grid[row][col];
+        if(prevcolor==color) return grid;
+        dfs(grid,row,col,prevcolor,color);
         for(auto p:internals){
             int row = p[0];
             int col = p[1];
-            cout<<row<<" "<<col<<endl;
-            grid[row][col]=originalColor;
+            grid[row][col]=prevcolor;
         }
         for(int i = 0;i<grid.size();i++){
             for(int j = 0;j<grid[0].size();j++){
